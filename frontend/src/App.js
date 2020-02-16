2db034cb18bec811f28a0ff142f507363ad98559
YoogottamK <yoogottamk@outlook.com>
2020-02-16 14:10:03 +0530

    added some more stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "./store";
 
 import Register from "./components/Register";
+import Login from "./components/Login";
+import Landing from "./components/Landing";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/App.css";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Route exact path="/register" component={Register} />
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Router>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
