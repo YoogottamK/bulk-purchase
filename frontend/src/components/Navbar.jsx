e83e0977d2d21979aff2cc062c7dd08d72754e12
YoogottamK <yoogottamk@outlook.com>
2020-02-18 12:03:32 +0530

    navbar changes links depending on user logged in state

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,38 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Button } from "react-bootstrap";
 import { connect } from "react-redux";
 import { userLogout } from "../actions/authActions";
 
+const LoggedInLinks = props => (
+  <>
+    <Nav.Link href="/home">Home</Nav.Link>
+    <Button onClick={props.logout}>Logout</Button>
+  </>
+);
+
+const LoggedOutLinks = () => (
+  <>
+    <Nav.Link href="/register">Register</Nav.Link>
+    <Nav.Link href="/login">Login</Nav.Link>
+  </>
+);
+
 class AppNavbar extends Component {
   render() {
-    const { user } = this.props.auth;
+    const { isAuthenticated } = this.props.auth;
 
     return (
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="/home">Home</Navbar.Brand>
+        <Navbar.Brand href="/home">BulkPurchase</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
+          <Nav className="ml-auto">
+            {isAuthenticated ? (
+              <LoggedInLinks logout={this.props.userLogout} />
+            ) : (
+              <LoggedOutLinks />
+            )}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
@@ -25,6 +43,10 @@ class AppNavbar extends Component {
 AppNavbar.propTypes = {
   userLogout: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
+};
+
+LoggedInLinks.propTypes = {
+  logout: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
