a5cf7bf0ca09301aa0eeb3113612dbbcc0bfa420
YoogottamK <yoogottamk@outlook.com>
2020-02-19 13:38:44 +0530

    correct http status code

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -21,7 +21,7 @@ router.post("/register", (req, res) => {
     const { errors, isValid } = validateRegisterInput(req.body);
 
     if (!isValid) {
-        return res.status(HttpStatusCodes.BAD_REQUEST).json(errors);
+        return res.status(HttpStatusCodes.UNPROCESSABLE_ENTITY).json(errors);
     }
 
     const { username, email, password, isVendor } = req.body;
@@ -64,7 +64,7 @@ router.post("/login", (req, res) => {
     const { errors, isValid } = validateLoginInput(req.body);
 
     if (!isValid) {
-        return res.status(HttpStatusCodes.BAD_REQUEST).json(errors);
+        return res.status(HttpStatusCodes.UNPROCESSABLE_ENTITY).json(errors);
     }
 
     const { email, password } = req.body;
